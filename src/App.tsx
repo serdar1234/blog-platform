89c3253d6534e3655828116dfca26e44ca012945
Serdar Geldyev <serdarabad@gmail.com>
2025-03-11 23:20:52 +0300

    test article added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Grid from "@mui/material/Grid2";
 import classes from "./App.module.scss";
-import ArticleCard from "./components/Card";
+import Article from "./components/Article";
+// import ArticleCard from "./components/Card";
+// import Pagination from "@mui/material/Pagination";
+// import Stack from "@mui/material/Stack";
 import Header from "./components/Header";
 import { Container } from "@mui/material";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
 
 function App() {
   return (
@@ -12,15 +13,16 @@ function App() {
       <Header />
       <Container style={{ maxWidth: 938 }}>
         <Grid rowGap={2} className={classes.grid}>
+          <Article />
+          {/* <ArticleCard />
           <ArticleCard />
           <ArticleCard />
           <ArticleCard />
-          <ArticleCard />
-          <ArticleCard />
+          <ArticleCard /> */}
         </Grid>
-        <Stack spacing={2} alignItems={"center"} sx={{ marginBottom: 2 }}>
+        {/* <Stack spacing={2} alignItems={"center"} sx={{ marginBottom: 2 }}>
           <Pagination count={10} shape="rounded" color="primary" />
-        </Stack>
+        </Stack> */}
       </Container>
     </>
   );

9e6933179eb900a8ab549954581da674f286da3f
Serdar Geldyev <serdarabad@gmail.com>
2025-03-13 14:38:17 +0300

    stringAvatar added

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -6,7 +6,12 @@ import { IArticleProps } from "../../types/interfaces";
 import convertDate from "../../utils/convertDate";
 import truncateStr from "../../utils/truncateStr";
 
+import { useState } from "react";
+import stringAvatar from "../../utils/stringAvatar";
+
 const Preview: React.FC<IArticleProps> = ({ info }) => {
+  const [imgState, setImgState] = useState<boolean>(false);
+
   return (
     <>
       <Grid component="div" size={9.6} style={{ height: "50%" }}>
@@ -32,7 +37,19 @@ const Preview: React.FC<IArticleProps> = ({ info }) => {
           <h6>{info.author.username}</h6>
           <time>{convertDate(info.createdAt)}</time>
         </div>
-        <Avatar src={info.author.image} alt={info.author.bio} />
+        {!imgState && (
+          <Avatar
+            {...stringAvatar(info.author.username)}
+            alt={info.author.bio}
+          />
+        )}
+        <Avatar
+          src={info.author.image}
+          onLoad={() => setImgState(true)}
+          onError={() => setImgState(false)}
+          alt={info.author.bio}
+          style={{ display: imgState ? "block" : "none" }}
+        />
       </Grid>
       <Grid component="div" size={9.6}>
         <Typography className={classes.text} component="span">

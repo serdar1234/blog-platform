b4e9fc298e5a1d52856917ef9c1ef58e87bc486c
Serdar Geldyev <serdarabad@gmail.com>
2025-08-03 19:33:36 +0300

    word wrap and string truncate refactored

--- src/utils/truncateStr.ts
+++ src/utils/truncateStr.ts
@@ -1,9 +1,17 @@
-export default function truncateStr(str: string, length = 52): string {
+const MAX_LENGTH = 52;
+const MAX_LENGTH_PREVIEW = 90;
+
+export default function truncateStr(str: string, length = MAX_LENGTH): string {
   if (str.length <= length) return str;
 
   let shortStr: string = str.slice(0, length);
   const lastSpaceIdx = shortStr.lastIndexOf(" ");
-  shortStr = shortStr.slice(0, lastSpaceIdx);
+
+  if (lastSpaceIdx === -1 || lastSpaceIdx > length) {
+    shortStr = shortStr.slice(0, MAX_LENGTH_PREVIEW);
+  } else {
+    shortStr = shortStr.slice(0, lastSpaceIdx);
+  }
 
   return `${shortStr}${length != 13 ? "..." : ""}`;
 }
